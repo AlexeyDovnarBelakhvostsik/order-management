047a1e0609fad72687e61785c993fe623443e408
GameRPG <tellur1993@gmail.com>
2025-01-31 10:41:55 +0300

    Исправлены мелкие ошибки связанные с тем что иногда отлетает Lombok

--- src/main/java/ru/itq/ordermanagement/entity/OrdersDetailsEntity.java
+++ src/main/java/ru/itq/ordermanagement/entity/OrdersDetailsEntity.java
@@ -1,6 +1,7 @@
 package ru.itq.ordermanagement.entity;
 
 
+import lombok.Getter;
 import lombok.Setter;
 
 /**
@@ -11,6 +12,8 @@ import lombok.Setter;
  * toString переопределен в этом же классе стандартным методом
  */
 
+@Getter
+@Setter
 public class OrdersDetailsEntity {
     private int id;
     private  int articleProduct;
@@ -24,54 +27,6 @@ public class OrdersDetailsEntity {
         this.nameProduct = nameProduct;
         this.quantity = quantity;
         this.unitCost = unitCost;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setArticleProduct(int articleProduct) {
-        this.articleProduct = articleProduct;
-    }
-
-    public void setNameProduct(String nameProduct) {
-        this.nameProduct = nameProduct;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setUnitCost(int unitCost) {
-        this.unitCost = unitCost;
-    }
-
-    public void setIdOrders(int idOrders) {
-        this.idOrders = idOrders;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getArticleProduct() {
-        return articleProduct;
-    }
-
-    public String getNameProduct() {
-        return nameProduct;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public int getUnitCost() {
-        return unitCost;
-    }
-
-    public int getIdOrders() {
-        return idOrders;
     }
 
     @Override
--- src/main/java/ru/itq/ordermanagement/entity/OrdersEntity.java
+++ src/main/java/ru/itq/ordermanagement/entity/OrdersEntity.java
@@ -1,6 +1,8 @@
 package ru.itq.ordermanagement.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -13,8 +15,10 @@ import java.util.List;
  * Геттеры и Сеттеры созданы библиотекой Lombok(Lombok иногда отваливается, поэтому пока по старинке созданы)
  * toString переопределен в этом же классе стандартным методом
  */
+@Setter
+@Getter
 @Data
-@Table(name = "orders",schema = "orders")
+@Table(name = "orders", schema = "orders")
 public class OrdersEntity {
     @Id
     private int id;
@@ -27,91 +31,7 @@ public class OrdersEntity {
     private String deliveryType;
     private List<OrdersDetailsEntity> ordersDetails;
 
-    public OrdersEntity(String orderNumber, int totalOrderAmount, LocalDate orderDate, String address, String deliveryAddress, String paymentType, String deliveryType, List<OrdersDetailsEntity> ordersDetails) {
-        this.orderNumber = orderNumber;
-        this.totalOrderAmount = totalOrderAmount;
-        this.orderDate = orderDate;
-        this.address = address;
-        this.deliveryAddress = deliveryAddress;
-        this.paymentType = paymentType;
-        this.deliveryType = deliveryType;
-        this.ordersDetails = ordersDetails;
-
-    }
-
     public OrdersEntity() {
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setOrderNumber(String orderNumber) {
-        this.orderNumber = orderNumber;
-    }
-
-    public void setTotalOrderAmount(int totalOrderAmount) {
-        this.totalOrderAmount = totalOrderAmount;
-    }
-
-    public void setOrderDate(LocalDate orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setDeliveryAddress(String deliveryAddress) {
-        this.deliveryAddress = deliveryAddress;
-    }
-
-    public void setPaymentType(String paymentType) {
-        this.paymentType = paymentType;
-    }
-
-    public void setDeliveryType(String deliveryType) {
-        this.deliveryType = deliveryType;
-    }
-
-    public void setOrdersDetails(List<OrdersDetailsEntity> ordersDetails) {
-        this.ordersDetails = ordersDetails;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getOrderNumber() {
-        return orderNumber;
-    }
-
-    public int getTotalOrderAmount() {
-        return totalOrderAmount;
-    }
-
-    public LocalDate getOrderDate() {
-        return orderDate;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getDeliveryAddress() {
-        return deliveryAddress;
-    }
-
-    public String getPaymentType() {
-        return paymentType;
-    }
-
-    public String getDeliveryType() {
-        return deliveryType;
-    }
-
-    public List<OrdersDetailsEntity> getOrdersDetails() {
-        return ordersDetails;
     }
 
     @Override
